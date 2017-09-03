5f0c98ec8d6abe6152bc60c633d890ec821aaf46
Jumpei Ogawa <phanective@gmail.com>
2017-09-03 17:42:00 +0900

    test: Also test tslint config if version is bumped up correctly

--- test.sh
+++ test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname "$0")"
 RED="\033[0;31m"
 NC="\033[0m" # No Color
 
 npx eslint .
 
-# version in package.json shouldn't be existing version on npmjs.com
-NEW_VERSION="$(cat package.json | jq --raw-output ".version")"
-if [[ -n "$(npm view "@phanect/eslint-config-phanective@$NEW_VERSION")" ]]; then
-  echo -e "$RED@phanect/eslint-config-phanective@$NEW_VERSION already exists. Did you update version in package.json?$NC\n" > /dev/stderr
-  exit 1
-fi
+check_version () {
+  local linter="$1"
+
+  NEW_VERSION="$(cat "$SCRIPT_DIR/$linter-config/package.json" | jq --raw-output ".version")"
+  if [[ -n "$(npm view "@phanect/$linter-config-phanective@$NEW_VERSION")" ]]; then
+    echo -e "$RED@phanect/$linter-config-phanective@$NEW_VERSION already exists. Did you update version in package.json?$NC\n" > /dev/stderr
+    exit 1
+  fi
+}
+
+check_version "eslint"
+check_version "tslint"
