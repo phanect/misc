e5e38dc79ec58f6fbb1d4e808c843ab44bcf2a76
Jumpei Ogawa <phanective@gmail.com>
2019-11-21 00:08:49 +0900

    refactor: use mergeConfigs() to merge rules

--- config.js
+++ config.js
@@ -1,26 +1,25 @@
 "use strict";
 
+const { mergeConfigs } = require("../helpers");
+
 const js = require("./configs/js");
 const ts = require("./configs/ts");
 const node = require("./configs/node");
 
-const react = (lang) => ({
-  extends: lang.extends.concat("plugin:react/recommended"),
-  plugins: lang.plugins.concat("react"),
-  parserOptions: Object.assign(lang.parserOptions, {
+const react = (lang) => mergeConfigs(lang, {
+  extends: [ "plugin:react/recommended" ],
+  plugins: [ "react" ],
+  parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-  }),
-  // Since first argument is modified, lang.rules must be the second argument,
-  // or js.rules includes rules for React or Vue
-  rules: Object.assign({}, lang.rules),
+  },
 });
 
-const vue = (lang) => ({
-  extends: lang.extends.concat("plugin:vue/recommended"),
-  plugins: lang.plugins.concat("vue"),
-  rules: Object.assign({
+const vue = (lang) => mergeConfigs(lang, {
+  extends: [ "plugin:vue/recommended" ],
+  plugins: [ "vue" ],
+  rules: {
     //
     // Warnings
     //
@@ -29,9 +28,7 @@ const vue = (lang) => ({
       singleline: 7,
       multiline: { max: 2 },
     }],
-    // Since first argument is modified, lang.rules must be the second argument,
-    // or js.rules includes rules for React or Vue
-  }, lang.rules),
+  },
 });
 
 module.exports = {
