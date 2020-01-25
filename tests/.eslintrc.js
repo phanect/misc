3d4338f2685e25a81362b8f716dd2f72bc8ffb66
Jumpei Ogawa <phanective@gmail.com>
2020-01-25 22:32:31 +0900

    chore: use Jest config of this plugin

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,16 +1,3 @@
 "use strict";
 
-module.exports = {
-  env: {
-    "jest/globals": true
-  },
-  plugins: [ "jest" ],
-
-  rules: {
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error"
-  }
-}
+module.exports = require("../configs/jest");
