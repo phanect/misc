b39c8df236fc652a50da047a952f8ca702e575d8
Jumpei Ogawa <phanective@gmail.com>
2020-07-08 17:11:50 +0900

    fix: ESLint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,9 @@ module.exports = mergeConfigs(require("./configs/js"), {
   root: true,
 
   env: {
-    node: true
+    node: true,
   },
   parserOptions: {
     project: "./tests/ts/tsconfig.json",
-  }
+  },
 });
