7d2960bee64c7c77e1dd18e2cf1141e2ccbfdfa5
Jumpei Ogawa <phanective@gmail.com>
2022-03-12 06:47:14 +0900

    feat: convert from eslint-plugin to eslint-config

--- scripts/builder.js
+++ scripts/builder.js
@@ -12,19 +12,19 @@ const nuxtJS = require("../src/configs/nuxt+js");
 const nuxtTS = require("../src/configs/nuxt+ts");
 const _jest = require("../src/configs/jest"); // do not name this variable "jest" to avoid name conflict on test
 
-const distDir = join(__dirname, "../dist/");
+const rootDir = join(__dirname, "../");
 
 (async () => {
-  await mkdir(distDir, { recursive: true });
+  await mkdir(rootDir, { recursive: true });
   await Promise.all([
-    writeFile(join(distDir, "plain.json"), JSON.stringify(plain, null, 2)),
-    writeFile(join(distDir, "node.json"), JSON.stringify(node, null, 2)),
-    writeFile(join(distDir, "react.json"), JSON.stringify(react, null, 2)),
-    writeFile(join(distDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
-    writeFile(join(distDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
-    writeFile(join(distDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
-    writeFile(join(distDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
-    writeFile(join(distDir, "jest.json"), JSON.stringify(_jest, null, 2)),
+    writeFile(join(rootDir, "plain.json"), JSON.stringify(plain, null, 2)),
+    writeFile(join(rootDir, "node.json"), JSON.stringify(node, null, 2)),
+    writeFile(join(rootDir, "react.json"), JSON.stringify(react, null, 2)),
+    writeFile(join(rootDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
+    writeFile(join(rootDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
+    writeFile(join(rootDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
+    writeFile(join(rootDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
+    writeFile(join(rootDir, "jest.json"), JSON.stringify(_jest, null, 2)),
   ]);
 
   console.log("JSONs are successfully generated");
