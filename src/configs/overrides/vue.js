03321b7c897a8199b329832368fa3549ce9d76e0
Jumpei Ogawa <phanective@gmail.com>
2023-08-26 12:26:44 +0900

    feat: update Vue rules

--- src/configs/overrides/vue.js
+++ src/configs/overrides/vue.js
@@ -18,10 +18,16 @@ module.exports = mergeConfigs(plain, {
     //
     // Warnings
     //
-    "vue/html-self-closing": [ "warn", { html: { normal: "never" }}],
+    "vue/html-self-closing": [ "warn", { html: { void: "always", normal: "never", component: "always" }}],
     "vue/max-attributes-per-line": [ "warn", {
       singleline: 7,
       multiline: { max: 2 },
     }],
+
+    //
+    // Off
+    //
+    "vue/multi-word-component-names": "off",
+    "vue/no-v-html": "off",
   },
 });
