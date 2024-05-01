e5bb8eece5ce969fe8fdacadfa9e7c1409e93753
Jumpei Ogawa <git@phanective.org>
2024-05-01 11:55:00 +0900

    TODO(test): use `exports` field of package.json

--- scripts/build.js
+++ scripts/build.js
@@ -12,19 +12,19 @@ import nuxtJS from "../src/configs/nuxt+js";
 import nuxtTS from "../src/configs/nuxt+ts";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const rootDir = join(__dirname, "../");
+const distDir = join(__dirname, "../dist");
 
 (async () => {
-  await mkdir(rootDir, { recursive: true });
+  await mkdir(distDir, { recursive: true });
   await Promise.all([
-    writeFile(join(rootDir, "plain.json"), JSON.stringify(plain, null, 2)),
-    writeFile(join(rootDir, "node.json"), JSON.stringify(node, null, 2)),
-    writeFile(join(rootDir, "react.json"), JSON.stringify(react, null, 2)),
-    writeFile(join(rootDir, "next.json"), JSON.stringify(next, null, 2)),
-    writeFile(join(rootDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
-    writeFile(join(rootDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
-    writeFile(join(rootDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
-    writeFile(join(rootDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
+    writeFile(join(distDir, "plain.json"), JSON.stringify(plain, null, 2)),
+    writeFile(join(distDir, "node.json"), JSON.stringify(node, null, 2)),
+    writeFile(join(distDir, "react.json"), JSON.stringify(react, null, 2)),
+    writeFile(join(distDir, "next.json"), JSON.stringify(next, null, 2)),
+    writeFile(join(distDir, "vue+js.json"), JSON.stringify(vueJS, null, 2)),
+    writeFile(join(distDir, "vue+ts.json"), JSON.stringify(vueTS, null, 2)),
+    writeFile(join(distDir, "nuxt+js.json"), JSON.stringify(nuxtJS, null, 2)),
+    writeFile(join(distDir, "nuxt+ts.json"), JSON.stringify(nuxtTS, null, 2)),
   ]);
 
   console.log("JSONs are successfully generated");
