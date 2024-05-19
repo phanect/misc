phanect/eslint-config-phanective

refactor: TypeScriptify

src/configs/next.ts
@@ -1,5 +1,5 @@
-import react from "./react";
-import { mergeConfigs } from "../helpers";
+import react from "./react.js";
+import { mergeConfigs } from "../helpers.js";
 
 export default mergeConfigs(react, {
   extends: [ "next/core-web-vitals" ],

src/configs/nuxt+js.ts
@@ -1,6 +1,6 @@
-import { jsRules } from "./overrides/lang-specific";
-import { nuxtBase } from "./overrides/nuxt";
-import { mergeConfigs } from "../helpers";
+import { jsRules } from "./overrides/lang-specific.js";
+import { nuxtBase } from "./overrides/nuxt.js";
+import { mergeConfigs } from "../helpers.js";
 
 export default mergeConfigs(nuxtBase, {
   overrides: [

src/configs/react.ts
@@ -1,5 +1,5 @@
-import plain from "./plain";
-import { mergeConfigs } from "../helpers";
+import plain from "./plain.js";
+import { mergeConfigs } from "../helpers.js";
 
 export default mergeConfigs(plain, {
   extends: [

src/configs/vue+js.ts
@@ -1,6 +1,6 @@
-import { jsRules } from "./overrides/lang-specific";
-import { vueBase } from "./overrides/vue";
-import { mergeConfigs } from "../helpers";
+import { jsRules } from "./overrides/lang-specific.js";
+import { vueBase } from "./overrides/vue.js";
+import { mergeConfigs } from "../helpers.js";
 
 export default mergeConfigs(vueBase, {
   overrides: [

tests/helpers.test.ts
@@ -1,4 +1,4 @@
-import { toTSRules, mergeConfigs } from "../src/helpers";
+import { toTSRules, mergeConfigs } from "../src/helpers.js";
 
 test("toTSRules", () => {
   const result = toTSRules({

phanect/misc

src/configs/next.ts
@@ -1,5 +1,5 @@
-import react from "./react";
-import { mergeConfigs } from "../helpers";
+import react from "./react.ts";
+import { mergeConfigs } from "../helpers.ts";
 
 export default mergeConfigs(react, {
   extends: [ "next/core-web-vitals" ],

src/configs/nuxt+js.ts
@@ -1,6 +1,6 @@
-import { jsRules } from "./overrides/lang-specific";
-import { nuxtBase } from "./overrides/nuxt";
-import { mergeConfigs } from "../helpers";
+import { jsRules } from "./overrides/lang-specific.ts";
+import { nuxtBase } from "./overrides/nuxt.ts";
+import { mergeConfigs } from "../helpers.ts";
 
 export default mergeConfigs(nuxtBase, {
   overrides: [

src/configs/react.ts
@@ -1,5 +1,5 @@
-import plain from "./plain";
-import { mergeConfigs } from "../helpers";
+import plain from "./plain.ts";
+import { mergeConfigs } from "../helpers.ts";
 
 export default mergeConfigs(plain, {
   extends: [

src/configs/vue+js.ts
@@ -1,6 +1,6 @@
-import { jsRules } from "./overrides/lang-specific";
-import { vueBase } from "./overrides/vue";
-import { mergeConfigs } from "../helpers";
+import { jsRules } from "./overrides/lang-specific.ts";
+import { vueBase } from "./overrides/vue.ts";
+import { mergeConfigs } from "../helpers.ts";
 
 export default mergeConfigs(vueBase, {
   overrides: [

tests/helpers.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import { toTSRules, mergeConfigs } from "../src/helpers";
+import { toTSRules, mergeConfigs } from "../src/helpers.ts";
 
 test("toTSRules", () => {
   const result = toTSRules({