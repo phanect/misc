91e2c0f99ac5462b333f50a7f9890b27ff27fd3a
Jumpei Ogawa <git@phanective.org>
2024-07-23 15:40:59 +0900

    tmp: migrate React and Next.js configs to flat config with FlatCompat

--- src/configs/next.ts
+++ src/configs/next.ts
@@ -1,6 +1,15 @@
-import deepmerge from "deepmerge";
-import react from "./react.ts";
+import { FlatCompat } from "@eslint/eslintrc";
+import { react } from "./react.js";
+import { projectRoot } from "../utils.ts";
+import type { Linter } from "eslint";
 
-export default deepmerge(react, {
-  extends: [ "next/core-web-vitals" ],
+const compat = new FlatCompat({
+  baseDirectory: projectRoot,
+  resolvePluginsRelativeTo: projectRoot,
 });
+
+export const next: Linter.FlatConfig[] = [
+  // plain is not required here because it is imported in `react()`
+  ...react,
+  ...compat.extends("next/core-web-vitals"),
+];
--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,19 +1,33 @@
-import deepmerge from "deepmerge";
+import { FlatCompat } from "@eslint/eslintrc";
+import { configs as reactConfigs } from "eslint-plugin-react";
 import plain from "./plain.ts";
+import { extensions, projectRoot } from "../utils.ts";
+import type { Linter } from "eslint";
 
-export default deepmerge(plain, {
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: [ "react", "react-hooks" ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+const compat = new FlatCompat({
+  baseDirectory: projectRoot,
+  resolvePluginsRelativeTo: projectRoot,
+});
+
+export const react: Linter.FlatConfig[] = [
+  ...plain,
+  reactConfigs.flat.recommended,
+  {
+    files: extensions,
+    rules: {
+      "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
+      "react/react-in-jsx-scope": "off",
     },
   },
-  rules: {
-    "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
-    "react/react-in-jsx-scope": "off",
-  },
-});
+  ...compat.config({
+    extends: [
+      "plugin:react-hooks/recommended",
+    ],
+    plugins: [ "react-hooks" ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  }),
+];
