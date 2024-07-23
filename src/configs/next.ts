70b353e277da285fe9b39ddec9c7cbcd3b445372
Jumpei Ogawa <git@phanective.org>
2024-07-23 15:52:11 +0900

    tmp: Revert "tmp: migrate React and Next.js configs to flat config with FlatCompat"
    This reverts commit 03c695c392c1b5fc11d5ada17ce041948eb8906e.

--- src/configs/next.ts
+++ src/configs/next.ts
@@ -1,15 +1,6 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { react } from "./react.js";
-import { projectRoot } from "../utils.ts";
-import type { Linter } from "eslint";
+import deepmerge from "deepmerge";
+import react from "./react.ts";
 
-const compat = new FlatCompat({
-  baseDirectory: projectRoot,
-  resolvePluginsRelativeTo: projectRoot,
+export default deepmerge(react, {
+  extends: [ "next/core-web-vitals" ],
 });
-
-export const next: Linter.FlatConfig[] = [
-  // plain is not required here because it is imported in `react()`
-  ...react,
-  ...compat.extends("next/core-web-vitals"),
-];
--- src/configs/react.ts
+++ src/configs/react.ts
@@ -1,33 +1,19 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { configs as reactConfigs } from "eslint-plugin-react";
+import deepmerge from "deepmerge";
 import plain from "./plain.ts";
-import { extensions, projectRoot } from "../utils.ts";
-import type { Linter } from "eslint";
 
-const compat = new FlatCompat({
-  baseDirectory: projectRoot,
-  resolvePluginsRelativeTo: projectRoot,
-});
-
-export const react: Linter.FlatConfig[] = [
-  ...plain,
-  reactConfigs.flat.recommended,
-  {
-    files: extensions,
-    rules: {
-      "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
-      "react/react-in-jsx-scope": "off",
+export default deepmerge(plain, {
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: [ "react", "react-hooks" ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-  ...compat.config({
-    extends: [
-      "plugin:react-hooks/recommended",
-    ],
-    plugins: [ "react-hooks" ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  }),
-];
+  rules: {
+    "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
+    "react/react-in-jsx-scope": "off",
+  },
+});
