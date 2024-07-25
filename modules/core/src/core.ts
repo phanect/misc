09363bafd6ed3b1ce2fe466c3bd6ec58cd9772ec
Jumpei Ogawa <git@phanective.org>
2024-07-25 09:26:00 +0900

    refactor: create configset for dev files

--- modules/core/src/core.ts
+++ modules/core/src/core.ts
@@ -2,6 +2,7 @@ import promise from "eslint-plugin-promise";
 import { jsConfigs, tsConfigs } from "./configs/defaults.ts";
 import { ignoreConfigs } from "./configs/ignores.ts";
 import { jsonConfigs } from "./configs/json.ts";
+import { devConfigs } from "./configs/nodejs.ts";
 import { vitestConfigs } from "./configs/vitest.ts";
 import type { Linter } from "eslint";
 import { vitestWorkaroundConfig } from "./vitest-workaround.js";
@@ -15,6 +16,7 @@ const plain: Linter.FlatConfig[] = [
   //importConfigs["flat/recommended"],
   //editorConfigConfigs["recommended"],
   //docWriteConfigs["recommended"],
+  ...devConfigs,
   ...vitestConfigs,
   ...jsonConfigs,
   vitestWorkaroundConfig,
