45b60e0f80790a0a5de2309b24927ce2cd931f24
Jumpei Ogawa <git@phanective.org>
2024-09-14 14:30:53 +0900

    refactor: remove unnecessary type cast
    This is warned by `@typescript-eslint/no-unnecessary-type-assertion`

--- modules/eslint/src/configs/svelte.ts
+++ modules/eslint/src/configs/svelte.ts
@@ -2,7 +2,7 @@ import svelte from "eslint-plugin-svelte";
 import type { Linter } from "eslint";
 
 const config: Linter.Config[] = [
-  ...(svelte.configs["flat/recommended"] as Linter.Config[]),
+  ...svelte.configs["flat/recommended"],
   {
     files: [ "**/*.svelte" ],
     rules: {
