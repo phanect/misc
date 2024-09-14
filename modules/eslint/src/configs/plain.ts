phanect/eslint-config-phanective

feat: set up configs for dev files

modules/eslint/src/configs/plain.ts
@@ -2,6 +2,7 @@ import stylistic from "@stylistic/eslint-plugin";
 import promise from "eslint-plugin-promise";
 import { ignoreConfigs } from "./overrides/ignores.ts";
 import { jsonConfigs } from "./overrides/json.ts";
+import { devConfigs } from "./overrides/nodejs.ts";
 import { vitestConfigs } from "./overrides/vitest.ts";
 import type { Linter } from "eslint";
 import { commonRules, jsRules, tsRules } from "./overrides/lang-specific.ts";
@@ -32,6 +33,7 @@ const plain: Linter.Config[] = [
   ...jsRules,
   ...tsRules,
   ...commonRules,
+  ...devConfigs,
   ...vitestConfigs,
   ...jsonConfigs,
 ] as const;

phanect/misc

modules/eslint/src/configs/plain.ts
@@ -1,4 +1,5 @@
 import { jsonConfigs } from "./components/json.ts";
+import { devConfigs } from "./components/nodejs.ts";
 import { vitestConfigs } from "./components/vitest.ts";
 import type { Linter } from "eslint";
 import { commonConfigs, jsConfigs, tsConfigs } from "./components/languages.ts";
@@ -23,6 +24,7 @@ const plain: Linter.Config[] = [
   ...jsConfigs,
   ...tsConfigs,
   ...commonConfigs,
+  ...devConfigs,
   ...vitestConfigs,
   ...jsonConfigs,
 ] as const;