22429c036a604ef1950a08bd76bea46723d68c90
Jumpei Ogawa <git@phanective.org>
2024-09-14 19:59:32 +0900

    test: add Astro example

---
+++ examples/astro/eslint.config.js
@@ -0,0 +1,18 @@
+import { core } from "eslint-config-phanective";
+import { astro } from "eslint-config-phanective/astro";
+
+/** @type { import("eslint").Linter.Config[] } */
+export default [
+  ...core,
+  ...astro,
+  {
+    languageOptions: {
+      parserOptions: {
+        // Do not use `projectService` because it causes error.
+        // https://github.com/ota-meshi/astro-eslint-parser/issues/331
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
