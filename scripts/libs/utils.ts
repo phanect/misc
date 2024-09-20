5c3d9ccbc1ab5062a543d319dd85697d6d1b4af1
Jumpei Ogawa <git@phanective.org>
2024-09-20 16:49:04 +0900

    feat(eslint): generate README.md to include in the `lint-*` packages

--- scripts/libs/utils.ts
+++ scripts/libs/utils.ts
@@ -1,12 +1,14 @@
 import { readdir } from "node:fs/promises";
 import { join } from "node:path";
 
-export const getModuleDirPaths = async (): Promise<string[]> => {
-  const modulesDirPath = join(import.meta.dirname, "../../modules");
+const modulesDirPath = join(import.meta.dirname, "../../modules");
 
-  return (await readdir(modulesDirPath))
+export const getModuleDirNames = async (): Promise<string[]> =>
+  readdir(modulesDirPath);
+
+export const getModuleDirPaths = async (): Promise<string[]> =>
+  (await getModuleDirNames())
     .map((moduleDirName) => join(modulesDirPath, moduleDirName));
-};
 
 export const getExampleDirPaths = async (): Promise<string[]> => {
   const examplesDirPath = join(import.meta.dirname, "../../examples");
