45d5f0e9c7f155d41e47ed67ea40a69eb835296b
Jumpei Ogawa <git@phanective.org>
2024-10-02 09:42:45 +0900

    style: lint

--- modules/lint/src/utils.ts
+++ modules/lint/src/utils.ts
@@ -6,7 +6,7 @@ export const toTSRules = (jsRules: Linter.RulesRecord): Linter.RulesRecord => {
   const tsRules: Linter.RulesRecord = {};
 
   for (const [ ruleid, options ] of Object.entries(jsRules)) {
-    tsRules[`@typescript-eslint/${ruleid}`] = options;
+    tsRules[`@typescript-eslint/${ ruleid }`] = options;
   }
 
   return tsRules;
