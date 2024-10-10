4605795e2638af03e15a82a02603c5edf9da3138
Jumpei Ogawa <git@phanective.org>
2024-10-11 07:17:48 +0900

    chore: migrate to pnpm

--- scripts/versions.ts
+++ scripts/versions.ts
@@ -25,29 +25,12 @@ const pkgJsons = await Promise.all(
   })),
 );
 
-const pkgNames = pkgJsons.map((pkgJson) => {
-  if (!pkgJson.content.name) {
-    throw new Error(`\`name\` property is not set in ${ pkgJson.path }.`);
-  }
-
-  return pkgJson.content.name;
-});
-
 const today = new Date();
 const newVersion = `${ today.getFullYear() }.${ today.getMonth() + 1 }.${ today.getDate() }`;
 
 for (const pkgJson of pkgJsons) {
   if (pkgJson.content.version) {
     pkgJson.content.version = newVersion;
-  }
-
-  for (const pkgName of pkgNames) {
-    if (pkgJson.content.dependencies?.[pkgName]) {
-      pkgJson.content.dependencies[pkgName] = newVersion;
-    }
-    if (pkgJson.content.devDependencies?.[pkgName]) {
-      pkgJson.content.devDependencies[pkgName] = newVersion;
-    }
   }
 
   await writeFile(pkgJson.path, JSON.stringify(pkgJson.content, null, 2) + "\n");
