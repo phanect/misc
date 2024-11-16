695b8015366b6a8630f07559cdd6d39d9302d2f9
Jumpei Ogawa <git@phanective.org>
2024-11-16 15:10:38 +0900

    feat: restore eslint-plugin-react-hooks

--- workspaces/lint-react/src/react.ts
+++ workspaces/lint-react/src/react.ts
@@ -1,15 +1,19 @@
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 import type { Linter } from "eslint";
 
 export const react: Linter.Config[] = [
   reactPlugin.configs.flat?.recommended as Linter.Config,
   reactPlugin.configs.flat?.["jsx-runtime"] as Linter.Config,
-  // TODO eslint-plugin-react-hooks
   jsxA11yPlugin.flatConfigs.recommended as Linter.Config,
 
   {
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
     rules: {
+      ...hooksPlugin.configs.recommended.rules,
       "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
       "react/react-in-jsx-scope": "off",
       "jsx-a11y/anchor-ambiguous-text": [ "error", {
