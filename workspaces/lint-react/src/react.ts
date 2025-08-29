3ad896c10df3a2ab0a81209a67f8982960da09c5
Jumpei Ogawa <git@phanective.org>
2025-08-30 06:52:21 +0900

    refactor: merge multiple config objects to a single config

--- workspaces/lint-react/src/react.ts
+++ workspaces/lint-react/src/react.ts
@@ -5,60 +5,59 @@ import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
 import hooksPlugin from "eslint-plugin-react-hooks";
 import globals from "globals";
-import type { Linter } from "eslint";
 
-export const react = defineConfig([
-  reactPlugin.configs.flat?.recommended,
-  reactPlugin.configs.flat?.["jsx-runtime"],
-  jsxA11yPlugin.flatConfigs.recommended,
-
-  {
-    plugins: {
-      "react-hooks": hooksPlugin,
-    },
-    rules: {
-      ...hooksPlugin.configs.recommended.rules,
-      "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
-      "react/react-in-jsx-scope": "off",
-      "jsx-a11y/anchor-ambiguous-text": [ "error", {
-        words: [
-          // Defaults
-          "click here",
-          "here",
-          "link",
-          "a link",
-          "learn more",
-
-          // Japanese
-          "ここをクリック",
-          "ここをタップ",
-          "ここ",
-          "こちらをクリック",
-          "こちらをタップ",
-          "こちら",
-          "リンク",
-          "詳細",
-        ],
-      }],
-    },
-  },
-].map((config) => ({
-  ...config,
+export const react = defineConfig([{
   files: [ "**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}" ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+
+  ...reactPlugin.configs.flat.recommended,
+  ...reactPlugin.configs.flat["jsx-runtime"],
+  ...jsxA11yPlugin.flatConfigs.recommended,
+
   languageOptions: {
-    globals: globals.browser,
+    ...reactPlugin.configs.flat.recommended.languageOptions,
+
+    globals: {
+      ...globals.browser,
+      ...globals.serviceworker,
+    },
+
     parserOptions: {
       ecmaFeatures: {
         jsx: true,
       },
     },
   },
-})));
+
+  plugins: {
+    react: reactPlugin,
+    "react-hooks": hooksPlugin,
+  },
+  rules: {
+    ...hooksPlugin.configs.recommended.rules,
+    "react/jsx-filename-extension": [ "error", { extensions: [ ".jsx", ".tsx" ]}],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-ambiguous-text": [ "error", {
+      words: [
+        // Defaults
+        "click here",
+        "here",
+        "link",
+        "a link",
+        "learn more",
+
+        // Japanese
+        "ここをクリック",
+        "ここをタップ",
+        "ここ",
+        "こちらをクリック",
+        "こちらをタップ",
+        "こちら",
+        "リンク",
+        "詳細",
+      ],
+    }],
+  },
+}]);
 
 const compat = new FlatCompat({
   baseDirectory: cwd(),
