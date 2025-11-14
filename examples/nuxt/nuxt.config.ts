3defe3fe3af69d4128be229d946b62053bbb4621
Jumpei Ogawa <git@phanective.org>
2025-11-14 11:57:48 +0900

    chore(example): update Nuxt's compatibility date

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2025-11-14",
   devtools: {
     enabled: true,
   },
