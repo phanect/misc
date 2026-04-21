b448300e2b5090a86217f74a06fa34f2afc50237
Jumpei Ogawa <git@phanective.org>
2026-04-21 10:33:58 +0900

    wip

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  bracketSameLine: false,
+  bracketSpacing: true,
+  proseWrap: "preserve",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
